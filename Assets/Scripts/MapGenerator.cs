9f55921e0d01dc04cf68d16cdd4e7c24ac8368d7
Chris Schnurr <ctschnurr@hotmail.com>
2023-10-10 09:50:16 -0300

    Completed Tutorial Ep. 2

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -29,6 +29,9 @@ public class MapGenerator : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             SmoothMap();
+
+            MeshGenerator meshGen = GetComponent<MeshGenerator>();
+            meshGen.GenerateMesh(map, 1);
         }
     }
 
@@ -102,17 +105,17 @@ public class MapGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-         if(map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
+        // if(map != null)
+        //{
+        //    for (int x = 0; x < width; x++)
+        //    {
+        //        for (int y = 0; y < height; y++)
+        //        {
+        //            Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+        //            Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+        //            Gizmos.DrawCube(pos, Vector3.one);
+        //        }
+        //    }
+        //}
     }
 }
