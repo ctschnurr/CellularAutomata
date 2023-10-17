e67443fd3f4e8eca95ffc7bc799ac372c2bf2965
Chris Schnurr <ctschnurr@hotmail.com>
2023-10-17 10:05:47 -0300

    Completed Tutorial Ep. 4

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -29,10 +29,25 @@ public class MapGenerator : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             SmoothMap();
-
-            MeshGenerator meshGen = GetComponent<MeshGenerator>();
-            meshGen.GenerateMesh(map, 1);
         }
+
+        int borderSize = 1;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+
+        for (int x = 0; x < borderedMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); y++)
+            {
+                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+                {
+                    borderedMap[x,y] = map[x-borderSize, y-borderSize];
+                }
+                else borderedMap[x,y] = 1;
+            }
+        }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(borderedMap, 1);
     }
 
     private void Update()
